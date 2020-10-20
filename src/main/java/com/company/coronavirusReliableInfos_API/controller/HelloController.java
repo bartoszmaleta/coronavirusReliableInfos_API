aa0430ecde4371c5e44871eca301f7ad09b258fa
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-20 16:36:10 +0200

    Add server test prints

--- src/main/java/com/company/coronavirusReliableInfos_API/controller/HelloController.java
+++ src/main/java/com/company/coronavirusReliableInfos_API/controller/HelloController.java
@@ -1,16 +1,35 @@
 package com.company.coronavirusReliableInfos_API.controller;
 
+import com.company.coronavirusReliableInfos_API.model.HelloMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
-    // localhost:8080/hello
+    // localhost:8085/hello
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello() {
         return "<h1>Hello, world!</h1>";
+    }
+
+    // localhost:8085/helloWithName?name=Bartosz
+    @RequestMapping(value = "/helloWithName", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloWithName(HttpServletRequest httpServletRequest) {
+        // get name parameter from request;
+        // will be null if no parameter passed in
+        String name = httpServletRequest.getParameter("name");
+
+        // if null, then "world"
+        if (name == null) {
+            name = "World!";
+        }
+
+        return String.format("<h1>%s</h1>", HelloMessage.getMessage(name));
     }
 }
---
+++ src/main/java/com/company/coronavirusReliableInfos_API/model/HelloMessage.java
@@ -0,0 +1,7 @@
+package com.company.coronavirusReliableInfos_API.model;
+
+public class HelloMessage {
+    public static String getMessage(String name) {
+        return String.format("Hello, %s !", name);
+    }
+}
