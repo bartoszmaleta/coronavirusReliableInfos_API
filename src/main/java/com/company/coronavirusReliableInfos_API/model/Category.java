d2f22a6507b26634db9efe31529186c75b271e9e
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-20 20:00:09 +0200

    Add Category model

---
+++ src/main/java/com/company/coronavirusReliableInfos_API/model/Category.java
@@ -0,0 +1,39 @@
+package com.company.coronavirusReliableInfos_API.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
