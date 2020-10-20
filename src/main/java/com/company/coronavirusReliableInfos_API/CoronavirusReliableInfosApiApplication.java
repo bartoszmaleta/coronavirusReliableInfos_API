6b02adcc97d57b602b23f3e44c56b9213fc8e653
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-20 16:33:23 +0200

    Initial commit

---
+++ src/main/java/com/company/coronavirusReliableInfos_API/CoronavirusReliableInfosApiApplication.java
@@ -0,0 +1,20 @@
+package com.company.coronavirusReliableInfos_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class CoronavirusReliableInfosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusReliableInfosApiApplication.class, args);
+	}
+	// localhost:8080/hello
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return "<h1>Hello, world!</h1>";
+	}
+}
---
+++ src/main/java/com/company/coronavirusReliableInfos_API/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.company.coronavirusReliableInfos_API.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    // localhost:8080/hello
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello() {
+        return "<h1>Hello, world!</h1>";
+    }
+}
