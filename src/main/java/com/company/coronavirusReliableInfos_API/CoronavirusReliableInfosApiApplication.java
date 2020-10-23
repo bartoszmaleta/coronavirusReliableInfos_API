5808d674089a050a9078ec990ed8d8c309c596ea
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-23 07:18:22 +0200

    Add swagger documentation

--- src/main/java/com/company/coronavirusReliableInfos_API/CoronavirusReliableInfosApiApplication.java
+++ src/main/java/com/company/coronavirusReliableInfos_API/CoronavirusReliableInfosApiApplication.java
@@ -2,11 +2,17 @@ package com.company.coronavirusReliableInfos_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableJpaRepositories("com.company.coronavirusReliableInfos_API/repository")
 public class CoronavirusReliableInfosApiApplication {
 
@@ -19,6 +25,13 @@ public class CoronavirusReliableInfosApiApplication {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "<h1>Hello, world!</h1>";
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.company.coronavirusReliableInfos_API")).build();
+
     }
 }
 
